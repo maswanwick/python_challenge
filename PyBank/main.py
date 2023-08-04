78b59dd42ca8e4de4dd9b4eee5a9bf10e535c06c
maswanwick <Matt.Swanwick@Cerner.com>
2023-08-04 09:18:18 -0500

    Added analysis formatting and general cleanup

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,7 +1,22 @@
 import csv
 
-# set data location
+# set data locations
 budget_data_path = "PyBank\\Resources\\budget_data.csv"
+analysis_data_path = "PyBank\\analysis\\pybank_analysis.txt"
+
+# creates a new file with no content, so we can append to it with our summary data
+def create_analysis_file():
+    f = open(analysis_data_path, "w")
+    f.close()
+
+# appends the summary data line to the analysis file as well as prints it to the console
+def write_content(content):
+    # append to analysis file
+    with open(analysis_data_path, "a") as analysis_file:
+        analysis_file.write(f"{content}\n")
+    
+    # print to console
+    print(content)
 
 # initializers
 total_months = 0
@@ -12,6 +27,8 @@ best_month_increase = ""
 best_month_decrease = ""
 best_month_increase_value = 0
 best_month_decrease_value = 0
+
+# main logic section
 
 # open the file
 with open(budget_data_path, encoding="UTF-8") as budget_data_csv:
@@ -33,7 +50,7 @@ with open(budget_data_path, encoding="UTF-8") as budget_data_csv:
         total_profit_or_loss += int(current_profit_or_loss)
 
         # if this isn't the first month, subtract the current profit/loss from the last profit/loss
-        # to get the monthly change.  Store that in profit changes list.
+        # to get the monthly change.  store that in profit changes list.
         # then test to see if the change is outside the current high or low marks.  if it is, 
         # store that value along with the month
         if (total_months > 1):
@@ -50,9 +67,16 @@ with open(budget_data_path, encoding="UTF-8") as budget_data_csv:
         # set the last profit/loss for the next iteration
         prev_profit_or_loss = current_profit_or_loss
 
-print(total_months)
-print(total_profit_or_loss)
-print(sum(profit_changes) / len(profit_changes))
-print(f"Increase: {best_month_increase} {best_month_increase_value}")
-print(f"Decrease: {best_month_decrease} {best_month_decrease_value}")
+# report our findings
 
+# create an empty file to hold our analysis
+create_analysis_file()
+
+# print the content to the analysis file and the console
+write_content("Financial Analysis")
+write_content("----------------------------")
+write_content(f"Total Months: {total_months}")
+write_content(f"Total: ${total_profit_or_loss}")
+write_content(f"Average Change: ${round(sum(profit_changes) / len(profit_changes), 2)}")
+write_content(f"Greatest Increase in Profits: {best_month_increase} (${best_month_increase_value})")
+write_content(f"Greatest Decrease in Profits: {best_month_decrease} (${best_month_decrease_value})")
